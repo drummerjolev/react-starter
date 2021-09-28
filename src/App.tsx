0654df06c6342c54d9afc3567323d7a2f58654b8
Jonathan Levi <jonathan1996levi@gmail.com>
2021-09-28 07:35:20 +0100

    [React] setup with favorites

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// HMJ
+import React from 'react';
+
+const App = (): React.ReactElement => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
